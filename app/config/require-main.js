6d4f2a6d5a7ab59ac3acd3972927a50d53d754d8
hefeng <245993596@qq.com>
2015-09-14 18:32:53 +0800

    [jrmb] 增加Main文件

--- app/config/require-main.js
+++ app/config/require-main.js
@@ -2,12 +2,24 @@
  * require.main
  */
 requirejs.config({
-    baseUrl: '/',
+    baseUrl: './',
+    //indexUrl: baseUrl+'/index',
     paths: {
-        'bootstrap': 'assets/js/bootstrap.js'
+        'jquery': 'libs/jquery-1.8.3.min',
+        'underscore': 'libs/underscore-1.8.3',
+        'backbone': 'libs/backbone-1.2.1',
+        'backbone.marionette': 'libs/backbone.marionette-2.4.1',
+        'backbone.localStorage': 'libs/backbone.localStorage-1.1.0'
+    },
+    shim: {
+        'backbone': ['underscore', 'jquery'],
+        'backbone.marionette': ['backbone'],
+        'backbone.localStorage': ['backbone']
     }
 });
 
-(function(undefined){
-    
-})();
+$(function(){
+    require(['app/main'], function(App){
+        console.log("App start!");
+    });
+});
---
+++ app/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by hefeng on 15-9-14.
+ */
+define(function(require, exports, module) {
+
+    require(['backbone'], function(Backbone){
+        console.log("hello backbone", Backbone);
+    })
+
+
+
+
+
+});
