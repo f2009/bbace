7aa3cfdf3c2ee8cb9b8cb855d6f0930415caaa0e
hefeng <245993596@qq.com>
2015-11-09 21:25:48 +0800

    增加编译文件

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    less = require("gulp-less"),
+    sass = require("gulp-sass"),
+    watch = require("gulp-watch"),
+    watchLess = require("gulp-watch-less"),
+    rename = require("gulp-rename")
+	;
+ 
+gulp.task('build-less', function () {
+    gulp.src('less/*.less')
+	.pipe(watchLess('less/*.less'))
+    .pipe(less())
+    .pipe(rename(function(path){
+		path.basename += ".less";
+	}))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('build-sass', function () {
+    gulp.src('sass/*.scss')
+	.pipe(watch('sass/*.scss'))
+    .pipe(sass())
+	.pipe(rename(function(path){
+		path.basename += ".scss";
+	}))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch-changed', function(){
+	gulp.watch();
+})
+
+gulp.task('default', ['build-less', 'build-sass']);
